9bf4d1d85524723820a79f6ec8969cc714bbbffe
Oncu <oohancan@gmail.com>
2022-07-08 21:02:42 +0300

    add attributedString

--- Weather/Views/Main/HomeView.swift
+++ Weather/Views/Main/HomeView.swift
@@ -22,21 +22,12 @@ struct HomeView: View {
             Image("House")
                 .frame(maxHeight: .infinity, alignment: .top)
                 .padding(.top, 257)
-            VStack {
+            VStack(spacing: -10) {
                 Text("Montreal")
                     .font(.largeTitle)
-                Spacer()
+                
                 VStack {
-//                    Text("19°" + "\n" + "Mostly Clear")
-                    Text("19°")
-                     .font(.system(size: 96, weight: .thin))
-                        .foregroundColor(.primary)
-                    +
-                    Text("\n")
-                    +
-                    Text("Mostly Clear")
-                        .font(.title3.weight(.semibold))
-                        .foregroundColor(.secondary)
+                    Text(attributedString)
                     
                     Text("H:24°   L:18°")
                         .font(.title3.weight(.semibold))
@@ -45,6 +36,23 @@ struct HomeView: View {
             }
             .padding(.top, 51)
         }
+    }
+    private var attributedString: AttributedString {
+        var string = AttributedString("19°" + "\n " + "Mostly Clear")
+        
+        if let temp = string.range(of: "19°") {
+            string[temp].font = .system(size: 96, weight: .thin)
+            string[temp].foregroundColor = .primary
+        }
+        if let pipe = string.range(of: " | ") {
+            string[pipe].font = .title3.weight(.semibold)
+            string[pipe].foregroundColor = .secondary
+        }
+        if let weather = string.range(of: "Mostly Clear") {
+            string[weather].font = .title3.weight(.semibold)
+            string[weather].foregroundColor = .secondary
+        }
+        return string
     }
 }
 

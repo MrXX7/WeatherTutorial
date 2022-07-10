99faba062a746cacb8ef9bb6f964a39fcaa918c5
Oncu <oohancan@gmail.com>
2022-07-10 21:06:04 +0300

    add func innerShadow

--- Weather/Views/Detail/ForecastView.swift
+++ Weather/Views/Detail/ForecastView.swift
@@ -18,7 +18,7 @@ struct ForecastView: View {
         .overlay {
 //            Mark Bottom Sheet Inner Shadow (Border)
             RoundedRectangle(cornerRadius: 44)
-                .stroke(.white, lineWidth: 1)
+                .stroke(Color.bottomSheetBorderMiddle, lineWidth: 1)
                 .blendMode(.overlay)
                 .offset(y: 1)
                 .blur(radius: 0)

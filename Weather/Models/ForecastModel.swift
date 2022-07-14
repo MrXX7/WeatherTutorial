a27587d3ac36eeae595ce54b68282b789f6c90da
Oncu <oohancan@gmail.com>
2022-07-14 12:46:20 +0300

    add ForecastCard and static daily

--- Weather/Models/ForecastModel.swift
+++ Weather/Models/ForecastModel.swift
@@ -64,4 +64,18 @@ extension Forecast {
         Forecast(date: .init(timeIntervalSinceNow: hour * 3), weather: .rainy, probability: 0, temperature: 19, high: 24, low: 18, location: "Montreal, Canada"),
         Forecast(date: .init(timeIntervalSinceNow: hour * 4), weather: .rainy, probability: 0, temperature: 19, high: 24, low: 18, location: "Montreal, Canada")
     ]
+    static let daily: [Forecast] = [
+        Forecast(date: .init(timeIntervalSinceNow: day * 0), weather: .rainy, probability: 30, temperature: 19, high: 24, low: 18, location: "Montreal, Canada"),
+        Forecast(date: .init(timeIntervalSinceNow: day * 1), weather: .rainy, probability: 0, temperature: 19, high: 24, low: 18, location: "Montreal, Canada"),
+        Forecast(date: .init(timeIntervalSinceNow: day * 2), weather: .windy, probability: 100, temperature: 19, high: 24, low: 18, location: "Montreal, Canada"),
+        Forecast(date: .init(timeIntervalSinceNow: day * 3), weather: .rainy, probability: 50, temperature: 18, high: 24, low: 18, location: "Montreal, Canada"),
+        Forecast(date: .init(timeIntervalSinceNow: day * 4), weather: .rainy, probability: 0, temperature: 19, high: 24, low: 18, location: "Montreal, Canada"),
+        Forecast(date: .init(timeIntervalSinceNow: day * 5), weather: .rainy, probability: 0, temperature: 19, high: 24, low: 18, location: "Montreal, Canada")
+        ]
+    static let cities: [Forecast] = [
+        Forecast(date: .now, weather: .rainy, probability: 0, temperature: 19, high: 24, low: 18, location: "Montreal, Canada"),
+        Forecast(date: .now, weather: .windy, probability: 0, temperature: 20, high: 21, low: 19, location: "Toronto, Canada"),
+        Forecast(date: .now, weather: .stormy, probability: 0, temperature: 13, high: 16, low: 8, location: "Tokyo, Japan"),
+        Forecast(date: .now, weather: .tornado, probability: 0, temperature: 23, high: 26, low: 16, location: "Tennesse, United States")
+    ]
 }

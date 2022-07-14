c3a32052288da1a01d78edf5aaca59b4fc8d1009
Oncu <oohancan@gmail.com>
2022-07-14 10:19:55 +0300

    add struct and extension Forecast

--- Weather/Models/ForecastModel.swift
+++ Weather/Models/ForecastModel.swift
@@ -21,3 +21,47 @@ enum Weather: String {
     case tornado = "Tornado"
     case windy = "Fast Wind"
 }
+
+struct Forecast: Identifiable {
+    var id = UUID()
+    var date: Date
+    var weather: Weather
+    var probability: Int
+    var temperature: Int
+    var high: Int
+    var low: Int
+    var location: String
+    
+    var icon: String {
+        switch weather {
+        case .clear:
+            return "Moon"
+        case .cloudy:
+            return "Cloud"
+        case .rainy:
+            return "Moon cloud mid rain"
+        case .stormy:
+            return "Sun cloud angled rain"
+        case .sunny:
+            return "Sun"
+        case .tornado:
+            return "Tornado"
+        case .windy:
+            return "Moon cloud fast wind"
+        }
+    }
+}
+
+extension Forecast {
+    static let hour: TimeInterval = 60 * 60
+    static let day: TimeInterval = 60 * 60 * 24
+    
+    static let hourly: [Forecast] = [
+        Forecast(date: .init(timeIntervalSinceNow: hour * -1), weather: .rainy, probability: 30, temperature: 19, high: 24, low: 18, location: "Montreal, Canada"),
+        Forecast(date: .now, weather: .rainy, probability: 0, temperature: 19, high: 24, low: 18, location: "Montreal, Canada"),
+        Forecast(date: .init(timeIntervalSinceNow: hour * 1), weather: .windy, probability: 0, temperature: 19, high: 24, low: 18, location: "Montreal, Canada"),
+        Forecast(date: .init(timeIntervalSinceNow: hour * 2), weather: .rainy, probability: 0, temperature: 19, high: 24, low: 18, location: "Montreal, Canada"),
+        Forecast(date: .init(timeIntervalSinceNow: hour * 3), weather: .rainy, probability: 0, temperature: 19, high: 24, low: 18, location: "Montreal, Canada"),
+        Forecast(date: .init(timeIntervalSinceNow: hour * 4), weather: .rainy, probability: 0, temperature: 19, high: 24, low: 18, location: "Montreal, Canada")
+    ]
+}

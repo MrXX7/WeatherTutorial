d4688707f31f1d68f3ab3969ee093f4c2033162d
Oncu <oohancan@gmail.com>
2022-07-14 19:11:23 +0300

    add struct WeatherWidget

---
+++ Weather/Views/Components/WeatherWidget.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherWidget.swift
+//  Weather
+//
+//  Created by Oncu Can on 14.07.2022.
+//
+
+import SwiftUI
+
+struct WeatherWidget: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WeatherWidget_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherWidget()
+    }
+}
--- Weather/Views/Detail/WeatherView.swift
+++ Weather/Views/Detail/WeatherView.swift
@@ -9,12 +9,22 @@ import SwiftUI
 
 struct WeatherView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+//            Mark Background
+            Color.background
+                .ignoresSafeArea()
+        }
+        .overlay {
+//            Mark Navigation Bar
+            NavigationBar()
+        }
+        .navigationBarHidden(true)
     }
 }
 
 struct WeatherView_Previews: PreviewProvider {
     static var previews: some View {
         WeatherView()
+            .preferredColorScheme(.dark)
     }
 }
